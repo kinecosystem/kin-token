faca897b838ec7184c4aa9084f0f3601c03e84de
Ory Band <oryband@gmail.com>
2017-09-05 19:34:28 +0300

    add ownable contract
    the ownable contract has an owner address,
    and provides basic authorization control functions,
    this simplifies and the implementation of "user permissions"
    
    includes tests

---
+++ contracts/Ownable.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.15;
+
+/// @title Ownable
+/// @dev The Ownable contract has an owner address, and provides basic authorization control functions, this simplifies
+/// and the implementation of "user permissions".
+contract Ownable {
+    address public owner;
+    address public newOwnerCandidate;
+
+    event OwnershipRequested(address indexed _by, address indexed _to);
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    /// @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    /// account.
+    function Ownable() {
+        owner = msg.sender;
+    }
+
+    /// @dev Reverts if called by any account other than the owner.
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert();
+        }
+
+        _;
+    }
+
+    modifier onlyOwnerCandidate() {
+        if (msg.sender != newOwnerCandidate) {
+            revert();
+        }
+
+        _;
+    }
+
+    /// @dev Proposes to transfer control of the contract to a newOwnerCandidate.
+    /// @param _newOwnerCandidate address The address to transfer ownership to.
+    function requestOwnershipTransfer(address _newOwnerCandidate) external onlyOwner {
+        require(_newOwnerCandidate != address(0));
+
+        newOwnerCandidate = _newOwnerCandidate;
+
+        OwnershipRequested(msg.sender, newOwnerCandidate);
+    }
+
+    /// @dev Accept ownership transfer. This method needs to be called by the perviously proposed owner.
+    function acceptOwnership() external onlyOwnerCandidate {
+        address previousOwner = owner;
+
+        owner = newOwnerCandidate;
+        newOwnerCandidate = address(0);
+
+        OwnershipTransferred(previousOwner, owner);
+    }
+}
